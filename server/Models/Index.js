79443c3c225db8e785ec9064d50e3e745a27d176
A.C.Kimari <annchristine704@gmail.com>
2021-09-01 04:11:23 -0700

    added and completed the models, and added seeds folder

---
+++ server/Models/Index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Teacher = require('./Teacher');
+const Parent = require('./Parent');
+const Student = require('./Student');
+
+
+module.exports = { User, Teacher, Parent, Student };
